c4d2981bea3ad373034b9e8871d6e5208d15b250
yanweijia <happyboyywj@163.com>
2016-09-22 21:56:00 +0800

    可运行

--- src/cn/yanweijia/ui/MainWindow.java
+++ src/cn/yanweijia/ui/MainWindow.java
@@ -9,15 +9,21 @@ import javax.swing.border.EmptyBorder;
 import jpcap.JpcapCaptor;
 import jpcap.NetworkInterface;
 import jpcap.NetworkInterfaceAddress;
+import jpcap.packet.Packet;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextPane;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 import java.awt.event.ActionEvent;
 import javax.swing.border.LineBorder;
+
+import java.awt.BorderLayout;
 import java.awt.Color;
+import javax.swing.JTextArea;
+import javax.swing.JScrollPane;
 
 public class MainWindow extends JFrame {
 	private NetworkInterface networkInterface;	//网卡接口
@@ -25,15 +31,15 @@ public class MainWindow extends JFrame {
 	private JLabel label_info;	//显示网卡信息的标签
 	private boolean isCapturing = false;	//是否正在抓包
 	private JPanel panel;
+	private JTextArea textArea;
+	private JpcapCaptor captor;	//用于与网络设备进行连接
+	
 	/**
 	 * @author weijia
 	 * @param index 网卡编号
 	 */
 	public MainWindow(int index) {
-		setTitle("抓包");
-		setVisible(true);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 999, 682);
+		
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -49,12 +55,34 @@ public class MainWindow extends JFrame {
 		JButton btn_cap = new JButton("开始抓包");
 		btn_cap.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				if(isCapturing==true){
-					btn_cap.setText("开始抓包");
-					isCapturing = false;
-				}else{
+				if(isCapturing==false){
 					btn_cap.setText("停止抓包");
 					isCapturing = true;
+					//TODO:抓包线程
+					new Thread(new Runnable(){
+						@Override
+						public void run() {
+							try {
+								captor = JpcapCaptor.openDevice(networkInterface, Integer.MAX_VALUE, false, 20);
+								while(isCapturing == true){
+									Packet packet = captor.getPacket();
+									if(packet!=null)	//如果抓包的数据不为空
+										textArea.append("\n"+packet);
+									
+									
+								}
+								System.out.println("停止抓包~~~");
+							} catch (IOException e1) {
+								e1.printStackTrace();
+							}
+						}
+					}).start();;
+					
+					
+					
+				}else{
+					btn_cap.setText("开始抓包");
+					isCapturing = false;
 				}
 				
 				
@@ -71,6 +99,22 @@ public class MainWindow extends JFrame {
 		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
 		panel.setBounds(10, 101, 963, 531);
 		contentPane.add(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		panel.add(scrollPane, BorderLayout.CENTER);
+		
+		textArea = new JTextArea();
+		textArea.setLineWrap(true);
+		scrollPane.setViewportView(textArea);	//textArea嵌套在ScrollPane里面放在面板中央
+		
+		
+		setResizable(false);
+		setTitle("抓包");
+		setVisible(true);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 999, 682);
+		//获取网卡信息并显示
 		init();
 	}
 	/**
